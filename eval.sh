0385471d1c7686bb644d9de37ebebc86ef0d2178
chLi <565353780@qq.com>
2023-02-13 19:04:07 +0800

    update train and eval commands

--- eval.sh
+++ eval.sh
@@ -1,7 +1,8 @@
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
   --nproc_per_node=1 train_cad_ddp.py \
   --data_root /home/chli/chLi/FloorPlanCAD \
-  --pretrained_model /home/chli/chLi/HRNet/hrnetv2_w48_imagenet_pretrained.pth \
+  --log_dir ./logs/test0/ \
+  --load_ckpt ./logs/train0/best_model.pth \
   --test_only
 
 # CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
   --nproc_per_node=1 train_cad_ddp.py \
   --data_root /home/chli/chLi/FloorPlanCAD \
-  --pretrained_model /home/chli/chLi/HRNet/hrnetv2_w48_imagenet_pretrained.pth
+  --pretrained_model /home/chli/chLi/HRNet/hrnetv2_w48_imagenet_pretrained.pth \
+  --log_dir ./logs/train0/ \
+  --max_prim 6000
 
