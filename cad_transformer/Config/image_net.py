b1b4762d806ed847d28585d4a7f6a3dc7bc895c5
chLi <565353780@qq.com>
2023-02-13 19:31:01 +0800

    add configs

---
+++ cad_transformer/Config/image_net.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STD = [0.229, 0.224, 0.225]
+
+INV_IMAGENET_MEAN = [-m for m in IMAGENET_MEAN]
+INV_IMAGENET_STD = [1.0 / s for s in IMAGENET_STD]
