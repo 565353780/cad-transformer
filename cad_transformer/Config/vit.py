91a0162dfd397b675532d7231c4c23f1bdc97b14
chLi <565353780@qq.com>
2023-02-14 20:59:23 +0800

    add models configs

---
+++ cad_transformer/Config/vit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+vit_stage_layer_mapping = {
+    "stage1": [0, 1],
+    "stage2": [2, 3],
+    "stage3": [4, 5, 6, 7, 8, 9],
+    "stage4": [10, 11]
+}
