4164d7092291c8df683e9a070a645bdb246c62a4
chLi <565353780@qq.com>
2023-02-14 22:13:15 +0800

    create basic training script

---
+++ cad_transformer/Demo/trainer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cad_transformer.Module.trainer import Trainer
+
+
+def demo():
+    trainer = Trainer()
+    trainer.train()
+    return True
--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
-CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
-  --nproc_per_node=1 train_cad_ddp.py \
-  --data_root /home/chli/chLi/FloorPlanCAD \
-  --pretrained_model /home/chli/chLi/HRNet/hrnetv2_w48_imagenet_pretrained.pth \
-  --log_dir train0 \
-  --max_prim 3000
+# CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
+  # --nproc_per_node=1 train_cad_ddp.py \
+  # --data_root /home/chli/chLi/FloorPlanCAD \
+  # --pretrained_model /home/chli/chLi/HRNet/hrnetv2_w48_imagenet_pretrained.pth \
+  # --log_dir train0 \
+  # --max_prim 3000
 
+CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch \
+  --nproc_per_node=1 demo.py
