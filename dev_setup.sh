20f2f0c2a251cfb960f040fe601648617ef88ff1
chLi <565353780@qq.com>
2023-02-14 19:51:02 +0800

    update setup bash files

--- dev_setup.sh
+++ dev_setup.sh
@@ -2,5 +2,5 @@ pip3 install torch torchvision torchaudio \
   --extra-index-url https://download.pytorch.org/whl/cu116
 
 pip install sklearn pillow opencv-python matplotlib \
-  scipy tqdm gdown svgpathtools pandas yacs lxml cairosvg
-
+  scipy tqdm gdown svgpathtools pandas yacs lxml \
+  cairosvg scikit-learn timm
