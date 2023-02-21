91086a181f5f74621b5543bdece3221538526de5
chLi <565353780@qq.com>
2023-02-21 14:05:58 +0800

    update model file path

--- cad_transformer/Demo/trainer.py
+++ cad_transformer/Demo/trainer.py
@@ -5,7 +5,7 @@ from cad_transformer.Module.trainer import Trainer
 
 
 def demo():
-    model_file_path = "./output/test/model_best.pth"
+    model_file_path = "./output/20230219_14:10:00/model_best.pth"
     print_progress = True
 
     trainer = Trainer()
