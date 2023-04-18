3ea78d474c52e2dc28ec05c4df6f8d7d58d143a3
chLi <565353780@qq.com>
2023-04-18 19:47:16 +0800

    update pytorch and cuda version

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 pip3 install torch torchvision torchaudio \
-  --extra-index-url https://download.pytorch.org/whl/cu116
+  --extra-index-url https://download.pytorch.org/whl/cu118
 
 pip install sklearn pillow opencv-python matplotlib \
   scipy tqdm gdown svgpathtools pandas yacs lxml \
